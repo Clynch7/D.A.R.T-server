c495d6c92df82b6753e65b52d9be03ee8732133a
Clynch7 <anton.silfver@gmail.com>
2018-12-15 19:02:58 +0100

    working server

--- server.js
+++ server.js
@@ -3,20 +3,55 @@
 var HTTP_PORT = 8080;
 
 var nodeStatic = require('node-static');
-var expess = require("express")
 var http = require('http');
 var https = require('https')
+var express = require('express');
+app = express();
 var fs = require('fs');
-var queue = require('queue')
-
+var queue = [];
 var i = 1;
-/**
- * Create servers for HTTPS and HTTP, have them serve the user with the requested file
- * There is a function inside solving strange error when geturl is /patient.html/THEROOM
- */
-var app = expess()
+
+var bodyParser     =         require("body-parser");
+var app            =         express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/getdart',function(req,res){
+    if(queue.length > 0){
+        var dart = queue.shift();
+        console.log(dart)
+        console.log(dart.score)
+        res.end(JSON.stringify({ answer : "accepted" , dart: dart.dart, score : dart.score}, null, 3))
+    }else{
+        res.end(JSON.stringify({ answer : "declined"}, null, 3))
+    }
+    
+});
+app.get('/dart',function(req,res){
+    res.sendFile(__dirname + '/DART.html')
+});
+app.post('/newdart',function(req,res){
+  var dart =req.body.dart
+  var score = req.body.score;
+  queue.push({"dart" : dart, "score" : score});
+
+  res.end("yes");
+});
+app.listen(8000,function(){
+  console.log("Started on PORT 8000");
+})
+
+/*
 var fileServer = new(nodeStatic.Server)();
 var httpServer = http.createServer(function(req, res) {
-    res.end(JSON.stringify({ answer : "accepted" , score : i }, null, 3))
-    i++;
-}).listen(HTTP_PORT);
+    console.log(req.url)
+    if(req.url === "/Dart.html"){
+        fileServer.serve(req, res);
+    }else if(req.url === "/newdart.html"){
+    }else{
+        res.end(JSON.stringify({ answer : "accepted" , score : i, url : req.url }, null, 3))
+        i++;
+    }
+    
+}).listen(HTTP_PORT);*/
